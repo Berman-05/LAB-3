58321aebe5ac0a44bfa7a2663f29ee9fd724d760
Berman-05 <bernardovelsquez145@gmail.com>
2024-08-12 09:35:00 -0600

    Agregar archivos de proyecto.

---
+++ LAB-3/RegularCLient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_3
+{
+    internal class RegularCLient: Client
+    {
+        public int RegisteredCars { get; set; }
+
+        public RegularCLient(string clientName, string clientEMail, string clientAdress, int registeredCars):base (clientName, clientEMail,clientAdress)
+        {
+            RegisteredCars = registeredCars;
+        }
+    }
+}
